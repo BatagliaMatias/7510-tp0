0aaffe4ce1a01e0923ca4ecbe236d74b7ea62b26
Matias Bataglia <matubat91@gmail.com>
2016-03-18 19:05:32 -0300

    llegaron a fumigar

---
+++ src/main/java/ar/fiuba/tdd/template/tp0/RandomNumber.java
@@ -0,0 +1,7 @@
+package ar.fiuba.tdd.template.tp0;
+
+/**
+ * Created by mbataglia on 18/03/16.
+ */
+public class RandomNumber {
+}
---
+++ src/test/java/ar/fiuba/tdd/template/tp0/InterpreterTest.java
@@ -0,0 +1,10 @@
+package ar.fiuba.tdd.template.tp0;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mbataglia on 18/03/16.
+ */
+public class InterpreterTest {
+
+}
